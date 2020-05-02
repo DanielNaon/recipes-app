c003c46e4e712d3f2537292b89e0ac08cbeb475b
daniel naon <danielnaon@gmail.com>
2020-05-02 18:59:30 +0300

    make it better

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -4,6 +4,7 @@ class Renderer{
     }
     renderData(newRecipes){
         console.log(newRecipes)
+        $("#recipe-container").empty
         const source = $("#recipe-template").html()
         const template = Handlebars.compile(source)
         const someHTML = template({recipe: newRecipes})
